0df3a846dc55496181377a82d5348beb3812bc9e
VictorLpzV <100876529+VictorLpzV@users.noreply.github.com>
2022-11-09 23:25:08 -0600

    Actualización 09/11/2022

---
+++ 03-counter-app-vite/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+    //document.createElement...
+    return (<h1>Hola Mundo, saludos!!!</h1>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
