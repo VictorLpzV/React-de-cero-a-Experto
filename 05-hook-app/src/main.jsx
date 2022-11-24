3b315939aed11c9bb2e8d5fe563bcd3a4eca957f
VictorLpzV <100876529+VictorLpzV@users.noreply.github.com>
2022-11-23 19:05:55 -0600

    Actualización 23/11/2022

---
+++ 05-hook-app/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
+//import { CounterApp } from './01-useState/CounterApp'
+//import { HooksApp } from './HooksApp'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FormWithCustomHook/>
+  </React.StrictMode>
+)
