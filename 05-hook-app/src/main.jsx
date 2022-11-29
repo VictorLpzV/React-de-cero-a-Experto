5d605eb5f2269600c316b012a720b83bee8f3617
VictorLpzV <100876529+VictorLpzV@users.noreply.github.com>
2022-11-28 21:43:19 -0600

    Actualización de actividades 28/11/2022

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -11,13 +11,17 @@ import { MemoHook } from './06-memos/MemoHook'
 import { Memorize } from './06-memos/Memorize'
 import { Padre } from './07-tarea-memo/Padre'
 import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { HooksApp } from './HooksApp'
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoApp/>
-  </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
